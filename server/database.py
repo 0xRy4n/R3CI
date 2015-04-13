0fe33dd002aafcb6c57c7e3948daf67a1cbd1ce0
Gordonzy <ryanjeffreygordon@gmail.com>
2015-04-13 13:52:06 -0400

    further progress on database

--- server/database.py
+++ server/database.py
@@ -4,11 +4,13 @@ class SQL:
 	conn, cur, usernm, passwd, host = None
 	connected = False
 
+	# Connects to MySQL
 	def connect(self):
 		self.conn = db.connect(username=self.usernm, passwd=self.passwd, host=self.host)
 		self.cur = self.conn.cursor()
 		self.connected = True
 
+	# Disconnects MySQL
 	def disconnect(self):
 		self.cur.close()
 		self.connected = False
@@ -19,16 +21,36 @@ class SQL:
 			self.connect()
 		else:
 			self.cur.execute(command)
-			
+
+	# Creates a database called 'name'
 	def createDB(self, name):
 		command = "CREATE DATABASE {};".format(name)
 		self.execute(command)
 
+	# Creates a table in 'database' called 'name'.
 	def createTable(self, name, database):
 		command = "USE {};".format(database)
 		command1 = "CREATE TABLE {};".format(name)
 		self.execute(command)
 		self.execute(command1)
+
+	# Creates a column under 'table' in 'database' called 'name'.
+	# Optional parameters:
+	#	 'length' to specify how many values to store if required (as varchar does for example).
+	#	 'after' to insert column after another column.
+	def createColumn(self, name, table, database, coltype="varchar", length=60, after=None):
+		types = ["char, varchar, text, bool, int, float"]
+
+		match = 0
+		for i in range(0, len(types) - 1):
+			if types[i].upper() == coltype.upper():
+				match = 1
+				if <= 1:
+					coltype = "{}({})".format(coltype, length)
+
+		if match:
+			command = "USE {};".format(database)
+			command1 = "ALTER TABLE {} ADD {} {};".format(table, name, coltype)
 
 	def __init__(self, username, password, hostnm="127.0.0.1"):
 		self.usernm = username
