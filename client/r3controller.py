90fc9c16b764c1a325225676f10fb5514d05be10
Gordonzy <ryanjeffreygordon@gmail.com>
2015-05-13 14:29:36 -0400

    finished prototype for controller's interaction with the coordinator

--- client/r3controller.py
+++ client/r3controller.py
@@ -2,43 +2,81 @@ import coordination, communication, collision
 
 class R3Controller:
 
+	# Object Instantiation #
 	coordinator = coordination.Coordinator()
 	communicator = communication.Communicator()
 	collision = collision.Collider()
 
-	def _requestCoord(UID):
-		retVal = False
+	def __init__(uniqueID):
+		self.uniqueID = uniqueID
 
+	# Private Functions #
+
+	def _requestCoord(self, UID):
+		retVal = False
 		# Format for a coordinate request of robot with UID
 		request = { 
-				UID : {
-				"get" : ["x", "y"] 
-					  }
+				UID :	{
+					"get" : ["x", "y"] 
+					 	}
 				  }
-
 		response = self.communicator.send("db", request)
 
 		if type(response) is dict:
-			
 			# TODO: Properly get x y values in a list from dict
 			coords = response["get"]
 			retVal = coords
 		else:
 			# Following assumes server will return a string containing a message if an error occurs.
 			print("Request failed. Server returned the following string:\n\n{}".format(response))		
-			# TODO: Add error handlign here
+			# TODO: Add error handling here
 		
 		return(retVal)
-			
-	def getAngleToRobot(self, curAngle, clientUID, targetUID):
+
+	def _requestAngle(self, UID):
 		retVal = False
 
-		curCoords = self._requestCoord(clientUID)
+		request = 	{
+				UID : 	{
+					"get" : ["angle"]
+						}
+					}
+		response = self.communicator.send("db", request)
+
+		if type(response) is dict:
+			# TODO: Properly get angle from dict
+			angle = response["get"]
+			retVal = angle
+		else: 
+			print("Request failed. Server returned the following string:\n\n{}".format(response))
+
+		return(retVal)
+
+
+	# Public Functions #
+	def getAngleToRobot(self, targetUID):
+		retVal = False
+
+		curCoords = self._requestCoord(self.uniqueID)
 		targCoords = self._requestCoord(targetUID)
 
 		angleToTarget = self.coordinator.getAngleToCoords(curAngle, curCoords, targCoords)
 
-		
+		if angleToTarget != False:
+			retVal = angleToTarget
 
+		return(retVal)
+
+
+	def getForwardCoords(self, distance):
+		retVal = False
+
+		curAngle = self._requestAngle(self.uniqueID)
+		forwardCoords = coordinator.calcForwardCoords(distance, curAngle)
+
+		if forwardCoords != False:
+			retVal = forwardCoords
+
+		return(retVal)
 
 
