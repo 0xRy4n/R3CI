56b9bf018377f4a9ade7a85033bfa98daaba19ec
CPWD <cpwd@CPWDLAP13.(none)>
2015-05-13 12:31:24 -0400

    continued progress on controller

--- client/r3controller.py
+++ client/r3controller.py
@@ -19,18 +19,26 @@ class R3Controller:
 		response = self.communicator.send("db", request)
 
 		if type(response) is dict:
-
 			
+			# TODO: Properly get x y values in a list from dict
+			coords = response["get"]
+			retVal = coords
+		else:
+			# Following assumes server will return a string containing a message if an error occurs.
+			print("Request failed. Server returned the following string:\n\n{}".format(response))		
+			# TODO: Add error handlign here
+		
+		return(retVal)
+			
+	def getAngleToRobot(self, curAngle, clientUID, targetUID):
+		retVal = False
+
+		curCoords = self._requestCoord(clientUID)
+		targCoords = self._requestCoord(targetUID)
+
+		angleToTarget = self.coordinator.getAngleToCoords(curAngle, curCoords, targCoords)
+
+		
 
 
-	def getAngleToRobot(self, clientUID, targUID):
-		# Request format for x and y coordinates of robot with UID
-		request = { 
-				UID : {
-				"get" : ["x", "y"] 
-						}
-				  
 
-		targCoords = self.communicator.send("db", request)
-
-		angleToRobot = self.coordinator.getAngleToCoords()
