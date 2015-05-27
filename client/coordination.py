f31b9ff2a286afa727f600636dd270150abc5964
Paul Longtine <paullongtine@gmail.com>
2015-05-27 09:21:07 -0400

    more stuffs

--- client/coordination.py
+++ client/coordination.py
@@ -3,18 +3,18 @@
 
     Copyright (C) R3CI Team :: All Rights Reserved
 
-    Foobar is free software: you can redistribute it and/or modify
+    R3CI is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    R3CI is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with R3CI.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import math
