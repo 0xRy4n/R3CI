06da72c70ee5711ab1e9157a908b86bbb9dc1381
Ryan <ryanjeffreygordon@gmail.com>
2015-06-03 14:12:09 -0400

    Update scribbot.py

--- client/scribbot.py
+++ client/scribbot.py
@@ -1,6 +1,8 @@
 """
     This file is part of R3CI.
+    
     Copyright (C) R3CI Team :: All Rights Reserved
+    
     R3CI is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
